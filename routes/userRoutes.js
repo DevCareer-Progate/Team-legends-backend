2afd1d20a1a49c90a3d54b6f722cecb7a3658e65
bcjcodes <blessingakpanemmanuel@gmail.com>
2020-06-15 08:41:41 +0100

    integrated login functionality and restructured validation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
-const express = require("express");
-const auth = require("../controller/auth");
-const router = express.Router();
+const express = require('express')
+const auth = require('../controller/auth')
+const router = express.Router()
 
 //signup route
-router.post("/user/signup", auth.signup);
+router.post('/user/signup', auth.signup)
+router.post('/user/login', auth.login)
 
-module.exports = router;
+module.exports = router
--- server.js
+++ server.js
@@ -1,42 +1,40 @@
-const express = require("express");
-
-const mongooose = require("mongoose");
-
-const app = express();
+const express = require('express')
+const mongooose = require('mongoose')
+const app = express()
 
 //require config.js and destructure
-const { database } = require("./config");
+const { database } = require('./config')
 
 //require route
-const userRoute = require("./routes/userRoutes");
+const userRoute = require('./routes/userRoutes')
 
 //adding express body parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // initialize database
 
 mongooose
   .connect(database, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
   })
-  .then((result) => {
-    console.log("Mongo Db is connected .......");
+  .then(result => {
+    console.log('Mongo Db is connected .......')
   })
-  .catch((err) => {
-    console.log(err);
-  });
+  .catch(err => {
+    console.log(err)
+  })
 
 // testing routes/endpoint
-app.get("/", function (req, res) {
-  res.send("hello");
-});
+app.get('/', function (req, res) {
+  res.send('hello')
+})
 
-app.use("/api/", userRoute);
+app.use('/api/v1', userRoute)
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
-});
+  console.log(`server is running on port ${port}`)
+})
