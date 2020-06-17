a6316afe371fa60ce75eadf4d564aab200947ad3
bcjcodes <blessingakpanemmanuel@gmail.com>
2020-06-17 17:44:28 +0100

    adding additional routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,8 @@ const router = express.Router()
 //signup route
 router.post('/user/signup', auth.signup)
 router.post('/user/login', auth.login)
+router.post('/user/reset-password', auth.checkToken, auth.resetPassword)
+router.post('/user/forget-password', auth.forgetPassword)
+router.post('/user/change-password/:token', auth.changePassword)
 
 module.exports = router
--- validation/login.js
+++ validation/login.js
@@ -1,7 +1,7 @@
 const Validator = require('validator')
 const isEmpty = require('./isEmpty')
 
-module.exports = function validateLoginInput (data) {
+module.exports = validateLoginInput = data => {
   let errors = {}
 
   data.email = !isEmpty(data.email) ? data.email : ''
